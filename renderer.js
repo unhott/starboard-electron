8cb4973f904060355910e94f36b430ed71ef22f5
unhott <41760384+unhott@users.noreply.github.com>
2021-05-26 15:08:22 -0400

    Hack in open file

--- renderer.js
+++ renderer.js
@@ -5,7 +5,17 @@
 // selectively enable features needed in the rendering
 // process.
 
+let StarboardEmbed = window.StarboardEmbed;
+
 window.myElectronPreload.setFileListener((event, fileContents) => {
-  let mountPoint = document.getElementById('mount-point');
-  console.log(mountPoint);
+  
+  const mount = document.querySelector("#mount-point");
+  mount.innerHTML = '';
+
+  const el = new StarboardEmbed({
+      notebookContent: fileContents,
+      src: "https://unpkg.com/starboard-notebook@0.9.4/dist/index.html"
+  });
+  el.style.width = "100%";
+  mount.appendChild(el);
 })
